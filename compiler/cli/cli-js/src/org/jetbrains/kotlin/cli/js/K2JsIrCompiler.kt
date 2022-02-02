4664f96c2a8d2843eebb0a5f3a93b8ae134811f3
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-02-02 13:30:55 +0300

    [JS IR] IR module to name

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
@@ -406,6 +406,7 @@ class K2JsIrCompiler : CLICompiler<K2JSCompilerArguments>() {
                         ir.context,
                         mainCallArguments,
                         relativeRequirePath = true,
+                        moduleToName = ir.moduleFragmentToUniqueName
                     )
 
                     transformer.generateModule(ir.allModules, setOf(TranslationMode.fromFlags(arguments.irDce, arguments.irPerModule)))
@@ -417,6 +418,7 @@ class K2JsIrCompiler : CLICompiler<K2JSCompilerArguments>() {
                         dceJs = arguments.irDce,
                         multiModule = arguments.irPerModule,
                         relativeRequirePath = true,
+                        moduleToName = ir.moduleFragmentToUniqueName
                     )
 
                     transformer.generateModule(ir.allModules)
